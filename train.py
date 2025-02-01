26c96810ee673d4badcec621cdd5a59b5b8cc972
ErikEk <erik.ek@gmail.com>
2025-02-01 08:48:07 +0100

    s

--- train.py
+++ train.py
@@ -1,65 +1,52 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+# hyperparameters
+batch_size = 4 # how many independent sequences will we process in parallel?
+block_size = 8 # What is the maximum context length for predictions?
+max_iters = 300000
+eval_interval = 300
+learning_rate = 1e-3
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+eval_iters = 200
+torch.manual_seed(1337)
+
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
-print(''.join(chars))
-print(vocab_size)
 
 # Try tiktoken later
 
-# Simple mapping/tokenizer
+# create a mapping from characters to integers
 stoi = {ch:i for i, ch in enumerate(chars)}
 itos = {i:ch for i, ch in enumerate(chars)}
 encode = lambda s: [stoi[c] for c in s] # encode: take a string, output a list f integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decode: take a list of integers, output a string
 
-print(encode("hi there"))
-print(decode(encode("hi there")))
-
-import torch
-import torch.nn as nn
-from torch.nn import functional as F
-torch.manual_seed(1337)
-
-if torch.cuda.is_available():
-    print(f"GPU: {torch.cuda.get_device_name(0)} is available.")
-else:
-    print("No GPU available. Training will run on CPU.")
-
-
-print(torch.__version__)  # Should return 2.4.0
-print(torch.cuda.is_available())  # Should return True
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
+#print(encode("hi there"))
+#print(decode(encode("hi there")))
 
 data = torch.tensor(encode(text), dtype=torch.long)
 data = data.to(device) # Remove?
-
-print(data.shape, data.dtype)
-print(data[:1000])
-
-
 n = int(0.9*len(data))
 train_data = data[:n]
 val_data = data[n:]
 
+#block_size= 8
+#train_data[:block_size+1]
 
-block_size= 8
-train_data[:block_size+1]
-
-x = train_data[:block_size]
+'''x = train_data[:block_size]
 y = train_data[1:block_size+1]
 for t in range(block_size):
     context = x[:t+1]
     target = y[t]
     print(f"When inpuit is {context} the target is: {target}")
+'''
 
-
-torch.manual_seed(1337)
-batch_size = 4 # how many independent squences will we process in parallel?
-block_size = 8 # What is the maximum context length for predictions?
 
 def get_batch(split):
     # Generate a small batch of data of inputs x and targets y
@@ -68,36 +55,31 @@ def get_batch(split):
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i:i+block_size] for i in ix])
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-    x = x.to(device)
-    y = y.to(device)
-    return x, y
 
-xb, yb = get_batch('train')
+    return x.to(device), y.to(device)
 
-print('inputs:')
-print(xb.shape)
-print(xb)
-print('targets:')
-print(yb.shape)
-print(yb)
-
-print('----')
-
-for b in range(batch_size): # batch dimension
-    for t in range(block_size): # time dimension
-        context = xb[b, :t+1]
-        target = yb[b,t]
-        print(f"when input is {context.tolist()} the target: {target}")
+@torch.no_grad() # No need because no backpropagration
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
 
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self, vocal_size):
+    def __init__(self, vocab_size):
         super().__init__()
         # each token directly reads of the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
 
     def forward(self, idx, targets=None):
-
         # idx and targets are both (B,T) tensor of integers
         logits = self.token_embedding_table(idx) # (B,T,C)
 
@@ -111,7 +93,7 @@ class BigramLanguageModel(nn.Module):
         return logits, loss
 
     def generate(self, idx, max_new_tokens):
-        # idx is (B,T) array of idices in the current context
+        # idx is (B,T) array of indices in the current context
         for _ in range(max_new_tokens):
             # Get the prdictions
             logits, loss = self(idx)
@@ -125,31 +107,34 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
 
-m = BigramLanguageModel(vocab_size)
-m.to(device) # Remove??
-logits, loss = m(xb,yb)
-print(logits.shape)
-print(loss)
+model = BigramLanguageModel(vocab_size).to(device)
+#logits, loss = m(xb,yb)
+#print(logits.shape)
+#print(loss)
 
 
-idx = torch.zeros((1,1), dtype=torch.long)
-idx = idx.to(device)
-print(decode(m.generate(idx, max_new_tokens=100)[0].tolist()))
+#idx = torch.zeros((1,1), dtype=torch.long)
+#idx = idx.to(device)
+#print(decode(m.generate(idx, max_new_tokens=100)[0].tolist()))
 
-optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
-batch_size = 32
-for steps in range(10000):
+for iter in range(max_iters):
+
+    # every now and then evaluate the loss on train and val sets
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
     # sample a batch of data
     xb, yb = get_batch('train')
 
     #evalutate the loss
-    logits, loss = m(xb, yb)
+    logits, loss = model(xb, yb)
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
 
-print(loss.item())
-idx = torch.zeros((1,1), dtype=torch.long)
-idx = idx.to(device)
-print(decode(m.generate(idx, max_new_tokens=300)[0].tolist()))
+
+context = torch.zeros((1,1), dtype=torch.long, device=device)
+print(decode(model.generate(context, max_new_tokens=300)[0].tolist()))
